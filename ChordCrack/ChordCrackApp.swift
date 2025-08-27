41ca6816df96ec4864c0f25cd1a03de1dcb065a5
Michael Maaseide <maaseidemichael@gmail.com>
2025-08-26 21:08:54 -0400

    Initial Commit

---
+++ ChordCrack/ChordCrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordCrackApp.swift
+//  ChordCrack
+//
+//  Created by Michael Maaseide on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ChordCrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChordCrackTests/ChordCrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChordCrackTests.swift
+//  ChordCrackTests
+//
+//  Created by Michael Maaseide on 8/26/25.
+//
+
+import Testing
+@testable import ChordCrack
+
+struct ChordCrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
